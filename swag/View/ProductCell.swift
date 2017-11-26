0cd847d7ab3438c88c06029141304715a74e311f
ervinpepic <ervin.hack@gmail.com>
2017-11-26 17:50:47 +0100

    Added buy cell

---
+++ swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  swag
+//
+//  Created by Ervin Pepic on 11/26/17.
+//  Copyright © 2017 Ervin Pepic. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
