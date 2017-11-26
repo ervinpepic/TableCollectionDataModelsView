f010b8bf1d777e3fc6e2b1ee9d6e748c399b43a5
ervinpepic <ervin.hack@gmail.com>
2017-11-26 16:10:15 +0100

    layout and logic for table done

--- swag/Controller/CategoriesVC.swift
+++ swag/Controller/CategoriesVC.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class CategoriesVC: UIViewController {
+class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var cateogryTable: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        cateogryTable.dataSource = self
+        cateogryTable.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.fetCategories().count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+            let category = DataService.instance.fetCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        }
+        else {
+            return CategoryCell()
+        }
+        
+    }
+
 
 
 }
---
+++ swag/Services/DataService.swift
@@ -0,0 +1,25 @@
+//
+//  DataService.swift
+//  swag
+//
+//  Created by Ervin Pepic on 11/26/17.
+//  Copyright © 2017 Ervin Pepic. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func fetCategories() -> [Category]{
+        return categories
+        
+    }
+}
